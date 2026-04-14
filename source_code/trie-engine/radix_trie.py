09715ba815d7e8398edf37a4fb08f4ef6bc760ee
Huy Phong <24521344@gm.uit.edu.vn>
2026-04-14 23:05:14 +0700

    Add RadixTree implementation and requirements

---
+++ source_code/trie-engine/radix_trie.py
@@ -0,0 +1,127 @@
+from typing import Dict, Optional, Any
+
+class RadixNode:
+    def __init__(self):
+        self.children = {}
+        self.flags = 00
+        self.meaning_offset = -1
+
+    def set_is_word(self):
+        self.flags |= 1
+        self.flags &= ~2
+
+    def is_word(self):
+        return self.flags & 1
+    
+    def set_deleted(self):
+        self.flags &= ~1
+        self.flags |= 2
+
+    def is_deleted(self):
+        return self.flags & 2
+
+class RadixTree:
+    def __init__(self):
+        self.root = RadixNode()
+
+    def insert(self, word, offset):
+        current_node = self.root
+
+        while word:
+            match_key = None
+            common_prefix = ""
+
+            for key in current_node.children:
+                i = 0
+
+                while i < min(len(key), len(word)) and key[i] == word[i]:
+                    i += 1
+
+                if i > 0:
+                    match_key = key 
+                    common_prefix = key[:i]
+                    break
+
+        if not match_key:
+            new_node = RadixNode()
+            new_node.set_is_word()
+            new_node.meaning_offset = offset
+            current_node.children[word] = new_node
+        elif common_prefix == match_key:
+            current_node = current_node.children[match_key]
+            word = word[len(common_prefix):]
+
+            if not word:
+                current_node.set_is_word()
+                current_node.meaning_offset = offset
+        else:
+            remaining_key = match_key[len(common_prefix):]
+            remaining_word = word[len(common_prefix):]
+
+            child_node = current_node.children.pop(match_key)
+            
+            split_node = RadixNode()
+            current_node.children[common_prefix] = split_node
+            split_node.children[remaining_key] = child_node
+
+            if remaining_word:
+                new_leaf = RadixNode()
+                new_leaf.set_is_word()
+                new_leaf.meaning_offset = offset
+                split_node.children[remaining_word] = new_leaf
+            else:
+                split_node.set_is_word()
+                split_node.meaning_offset = offset
+
+    def search(self, word):
+        current_node = self.root
+
+        while word:
+            match_key = None
+
+            for key in current_node.children:
+                if word.startswith(key):
+                    match_key = key 
+                    break
+
+            if not match_key:
+                return None
+            
+        if current_node.is_word() and not current_node.is_deleted():
+            return current_node.meaning_offset
+        return -1
+
+    def delete(self, word):
+        current_node = self.root
+
+        while word:
+            match_key = None
+
+            for key in current_node.children:
+                if word.startswith(key):
+                    match_key = key 
+                    break
+
+            if not match_key:
+                return False
+            
+        if current_node.is_word:
+            current_node.set_deleted()
+            return True
+        return False
+    
+    def to_dict(self):
+        def _traverse(node, path_label):
+            data = {
+                "name": path_label if path_label else "root",
+                "is_word": node.is_word(),
+                "is_deleted": node.is_deleted(),
+                "offset": node.meaning_offset if node.is_word() else -1,
+                "children": []
+            }
+
+            for key, child in node.children.items():
+                data["children"].append(_traverse(child, key))
+            return data
+
+        return _traverse(self.root, "")
